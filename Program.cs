e2a1dcf07ab79642af034000c6c3de3af2df3e01
ny20szalais <ny20szalais@203-11>
2024-11-29 10:20:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CA20241129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
